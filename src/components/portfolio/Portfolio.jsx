281a9d9bf3a73e7364b050b3b8b88cc99477c05d
kadaver-layout <ich@hasslich.ru>
2025-07-14 14:17:42 +0300

    законгчить настройку декораций

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,7 +4,7 @@ import styles from "./Portfolio.module.sass";
 import Image from "next/image";
 import Link from "next/link";
 import data from "@/data/data.json";
-import { MouseParallax } from "react-just-parallax";
+import { MouseParallax, ScrollParallax } from "react-just-parallax";
 
 const Portfolio = () => {
   const portfolio = data.portfolio;
@@ -17,19 +17,23 @@ const Portfolio = () => {
           {
             portfolio.projects.map((item, index) => (
               <li className={styles.item} key={index}>
-                <div className={styles.content}>
-                  <div className={styles.imgbox}>
-                    {/* <MouseParallax
-                      strength={item.strength}
-                      lerpEase={item.lerpEase}
-                      enableOnTouchDevice={true}
-                      shouldPause={false}
-                      isVertical={true}
-                      isAbsolutelyPositioned={true}
-                      scrollContainer={styles.about}
-                    > */}
+                <ScrollParallax
+                  strength={item.strength}
+                  lerpEase={0.07}
+                >
+                  <div className={styles.content}>
+                    {
+                      item.decoration ? (
+                        item.decoration.map((decoration, index) => (
+                          <div key={index} data-decoration={decoration.name}
+                            className={`${styles.decoration}  ${styles[`${decoration.class}`]}`}>
+                          </div>
+                        ))
+                      ) : null
+                    }
 
 
+                    <div className={styles.imgbox}>
                       <Image
                         src={item.img}
                         alt={item.title}
@@ -37,53 +41,52 @@ const Portfolio = () => {
                         height={250}
                         className={styles.img}
                       />
-
-                    {/* </MouseParallax> */}
-
-                  </div>
-                  <div className={styles.card}>
-                    <h2 className={styles.card_title}>
-                      {item.title}
-                    </h2>
-                    <p className={styles.card_desc}>
-                      {item.desc}
-                    </p>
-                    <div className={styles.card_links}>
-                      {
-                        item.links.map((link, index) => (
-                          link.type === "site" ? (
-                            <Link target="_blank" href={`https://${link.link}`} key={index} className={styles.card_link} >
-                              <Image
-                                src={link.favicon}
-                                alt=""
-                                width={20}
-                                height={20}
-                                className={styles.card_link_img}
-                              />
-                              <p className={styles.site}>{link.link}</p>
-                            </Link>
-                          )
-                            :
-                            link.type === "github" ? (
-                              <Link href={link.link} key={index} className={styles.card_link}>
+                    </div>
+                    <div className={styles.card}>
+                      <h2 className={styles.card_title}>
+                        {item.title}
+                      </h2>
+                      <p className={styles.card_desc}>
+                        {item.desc}
+                      </p>
+                      <div className={styles.card_links}>
+                        {
+                          item.links.map((link, index) => (
+                            link.type === "site" ? (
+                              <Link target="_blank" href={`https://${link.link}`} key={index} className={styles.card_link} >
                                 <Image
-                                  src="/img/git.svg"
+                                  src={link.favicon}
                                   alt=""
                                   width={20}
                                   height={20}
                                   className={styles.card_link_img}
                                 />
-                                <p className={styles.card_link_git}>{link.link}</p>
+                                <p className={styles.site}>{link.link}</p>
                               </Link>
-                            ) : null
-                        ))
-                      }
+                            )
+                              :
+                              link.type === "github" ? (
+                                <Link href={link.link} key={index} className={styles.card_link}>
+                                  <Image
+                                    src="/img/git.svg"
+                                    alt=""
+                                    width={20}
+                                    height={20}
+                                    className={styles.card_link_img}
+                                  />
+                                  <p className={styles.card_link_git}>{link.link}</p>
+                                </Link>
+                              ) : null
+                          ))
+                        }
+                      </div>
+
                     </div>
 
+
+
                   </div>
-
-                </div>
-
+                </ScrollParallax>
               </li>
             ))
           }
